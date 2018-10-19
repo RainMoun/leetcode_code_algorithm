e41374d24cc075b8c7d1a0c0585771ef82a4ff84
RainMoun <775653143@qq.com>
2018-10-19 15:43:35 +0800

    丑数

---
+++ 263_丑数.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isUgly(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num <= 0:
+            return False
+        else:
+            while num % 2 == 0:
+                num /= 2
+            while num % 3 == 0:
+                num /= 3
+            while num % 5 == 0:
+                num /= 5
+            if num == 1:
+                return True
+            else:
+                return False
+
+
+s = Solution()
+print(s.isUgly(6))
+
